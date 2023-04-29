e94b60de6924ab49b1cea1b769d2723e661965d8
ShiqiWang(s3754487) <126866244+Vitamin56@users.noreply.github.com>
2023-04-29 19:42:42 +1000

    Update ppd.cpp
    finished REQ1: Command-line arguments

--- ppd.cpp
+++ ppd.cpp
@@ -10,6 +10,25 @@ int main(int argc, char **argv)
 {
     /* validate command line arguments */
     // TODO
+    
+    // Check if 3 command line parameters have been entered
+    if (argc != 3){
+        std::cerr << "Error:invalid arguments passed in. " << std::endl;
+        std::cerr << "Correct arguments are:"<< std::endl << argv[0] << " <stockfile> <coinsfile>" << std::endl;
+        std::cerr << "Where <stockfile> and <coinfile> are two valid files in the expected format." << std::endl;
+        return EXIT_FAILURE; 
+    }
+
+    std::ifstream stockFile(argv[1]);
+    if (!stockFile) {
+    std::cerr << "Error: Failed to open stock file." << std::endl;
+    return EXIT_FAILURE;
+    } 
+    std::ifstream coinsFile(argv[2]);
+    if (!coinsFile) {
+    std::cerr << "Error: Failed to open coins file." << std::endl;
+    return EXIT_FAILURE;
+    } 
 
     std::cout << "Just a test, nothing implemented yet!" << std::endl;
     
