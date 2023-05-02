ffba115818e7581dc429a5dedce1ebb29cbbbd0c
MattRig7 <mattrigo2872@gmail.com>
2023-05-02 15:04:54 +1000

    implemented pesudocode for resetting stock count
    Signed-off-by: MattRig7 <mattrigo2872@gmail.com>

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,4 +1,9 @@
 #include "LinkedList.h"
+#include <iostream>
+using std::string;
+using std::vector;
+
+
 
 LinkedList::LinkedList() {
    head = nullptr;
@@ -8,4 +13,12 @@ LinkedList::LinkedList() {
 
 LinkedList::~LinkedList() {
     // TODO
+}
+
+void Stock::ResetStock(Stock* stock){
+
+    //loops through linkedlist
+    //sets all stock::on_hand values to DEFAULT_STOCK_LEVEL 20
+
+    std::cout << "“All stock has been reset to the default level of " << DEFAULT_STOCK_LEVEL << "”" << std::endl;
 }
