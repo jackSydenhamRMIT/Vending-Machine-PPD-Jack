5327880cb60e1896bffa8b93c10dca9b127fcbd8
ShiqiWang(s3754487) <126866244+Vitamin56@users.noreply.github.com>
2023-05-02 14:57:50 +1000

    Update Coin.cpp

--- Coin.cpp
+++ Coin.cpp
@@ -1,3 +1,13 @@
 #include "Coin.h"
+
+ Coin::Coin() {
+    denom = FIVE_CENTS;
+    count = 0;
+}
+
+Coin::Coin(Denomination denomination, unsigned count) {
+    denom = denomination;
+    this->count = count;
+}
  
  // implement functions for managing coins; this may depend on your design.
