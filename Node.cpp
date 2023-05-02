0d935bc7fc0cbdf6a12641f2a444e96a63e61a0f
MattRig7 <mattrigo2872@gmail.com>
2023-05-02 18:35:03 +1000

    removed unrequired method from node.cpp
    Signed-off-by: MattRig7 <mattrigo2872@gmail.com>

--- Node.cpp
+++ Node.cpp
@@ -10,10 +10,3 @@ Node::~Node(){
     // TODO
 };
 
-void Coin::ResetCoins(Coin* coin){
-
-    //resets coins to correct denomination
-    //DEFAULT_COIN_COUNT 20
-
-    std::cout << "“All coins has been reset to the default level of " << DEFAULT_COIN_COUNT << "”" << std::endl;
-}
